550cb32049ee5dbd476bcb4619fbcb3f5c16a74b
Jean-Philippe Bernardy <jean-philippe.bernardy@tweag.io>
2016-09-19 09:01:59 +0200

    motivation

---
+++ Examples/Protocol.hs
@@ -0,0 +1,39 @@
+-- This example is inspired from Lindley and Morris, "embedding
+-- session types in Haskell".
+
+-- It demonstrates how to encode protocols using linear types, in the
+-- flavour that we propose.
+
+{-# LANGUAGE TypeOperators #-}
+module Calculator where
+import Data.Monoid
+
+-- Broken man's linear types.
+type a ⊗ b = (a,b)
+type a ⊸ b = a -> b
+
+type Effect = IO () -- for simplicity
+-- instance Monoid Effect
+
+pr :: Double -> Effect -- "prints" a number
+pr = print
+
+type N a = a ⊸ Effect
+
+data Client
+  = Mul Double Double (N (Double ⊗ Server)) -- Client sends two 'Double' and
+                                            -- expects a double and a new server
+                                            -- session.
+  | Terminate
+type Server = N Client
+
+
+exampleClient :: N (N Client)
+exampleClient server = server $ Mul 12 34 $ \(product,server') ->
+  print product <> server' Terminate
+
+exampleServer :: Server
+exampleServer client = case client of
+  Mul x y k -> k (x*y,exampleServer)
+  Terminate -> return ()
+
