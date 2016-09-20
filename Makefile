0f8fade8e7c78d9ee4f6b016772c549d55ffea1d
Jean-Philippe Bernardy <jean-philippe.bernardy@tweag.io>
2016-09-20 11:21:56 +0200

    bibliography

--- Makefile
+++ Makefile
@@ -1,7 +1,13 @@
 all: FormalSystem.pdf
 
-%.pdf: %.tex
-	xelatex $<
+PaperTools/bibtex/jp.bib:
+	echo "Get the submodules"
+        false
+
+%.pdf: %.tex PaperTools/bibtex/jp.bib
+	xelatex $*
+	biber $*
+	xelatex $*
 
 %.tex: %.org
 	bin/org-to-tex.sh $*.org
--- shell.nix
+++ shell.nix
@@ -3,9 +3,13 @@ let orgEmacs = emacsWithPackages (with emacsPackagesNg; [org]);
 in stdenv.mkDerivation {
   name = "docsEnv";
   buildInputs = [ # orgEmacs
+                  biber
                   (texlive.combine {
                     inherit (texlive)
-                      scheme-small wrapfig marvosym wasysym wasy cm-super unicode-math filehook lm-math capt-of;
+                      biblatex
+                      logreq
+                      scheme-small wrapfig marvosym wasysym wasy cm-super unicode-math filehook lm-math capt-of
+                      xstring ucharcat;
                   })
                 ];
 }
