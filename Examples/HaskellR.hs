70453f6d959a22cd12ecf61ef37f0c0ed73816f5
Jean-Philippe Bernardy <jean-philippe.bernardy@tweag.io>
2016-09-20 10:14:10 +0200

    haskellR example

---
+++ Examples/HaskellR.hs
@@ -0,0 +1,48 @@
+-- This module demonstrates how to interface with an external library,
+-- such as R.
+
+module HaskellR where
+
+
+-- The R API is not re-entrant, so we will use an R-Context, which is unique:
+type RContext
+-- The above type is abstract and has no runtime representation in Haskell (it has one in R)
+
+
+-- An R operation will need an instance of this context, do work (in
+-- IO), and finally return release the context and return a result:
+type R a = RContext ⊸ IO (RContext ⊗ a)
+
+-- This type has a unit and a bind --- albeit linear ones, so it's not
+-- an instance of Monad.
+
+return x = \ctx -> return (ctx, x)
+k >>= f = \ctx -> do
+  (ctx,x) <- k
+  f x ctx
+
+
+-- If we were too add a primitive allowing to run an arbitrary R
+-- operation, then we may endup with several contexts (if we combine
+-- it with forkIO)
+
+-- withR :: R a ⊸ IO a -- wrong
+
+
+-- so make sure that we call this operation once only:
+withR ::1 R a ⊸ IO a
+
+-- alternatively, we could provide a single context and a way to discard it:
+rContext ::1 RContext
+doneWithR :: RContext ⊸ () -- not necessarily unique
+
+
+-- note that the idea generalises to arbitrary 'single contexts':
+type S ctx a = ctx ⊸ (ctx ⊗ a)
+
+-- and we could even define
+type IO = S #RealWorld
+
+
+
+
