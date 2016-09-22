04c5d905e9231efeb2e38ce6fd52653418e1a44c
Jean-Philippe Bernardy <jean-philippe.bernardy@tweag.io>
2016-09-22 10:30:37 +0200

    comment examples

--- Examples/Protocol.hs
+++ Examples/Protocol.hs
@@ -15,13 +15,15 @@ type a ⊸ b = a -> b
 type Effect = IO () -- for simplicity
 -- instance Monoid Effect
 
-pr :: Double -> Effect -- "prints" a number
-pr = print
-
+-- A linear negation that represents the transfer of control from
+-- server to client, and vice-versa.
 type N a = a ⊸ Effect
 
+-- | Protocol from the point of view of the client:
 data Client where
-  Mul :: Double -> Double -> (N (Double ⊗ Server)) -> Client
+  Mul :: Double -> Double -> -- ^ input for the request the request
+         {-1-}(N (Double ⊗ Server)) -> -- ^ what the server does to respond: it returns a Double, and a new server state. 
+         Client
          -- Client sends two 'Double' and
          -- expects a double and a new server
          -- session.
@@ -29,12 +31,28 @@ data Client where
 type Server = N Client
 
 
+-- The linearity of client/server states ensures that:
+
+-- 1. The protocol is respected. So, the effects that inevitably come
+-- into play in a real implementation (database, ...) will never 'get
+-- lost' or be duplicated.
+
+-- 2. The implementation won't 'hold onto' stale server/client states
+-- any longer than strictly necessary.
+
+-- In sum, linearity comes at the expense of backtracking in the
+-- protocol. Arguably this is exactly what one wants in many
+-- applications.
+
 exampleClient :: N (N Client)
-exampleClient server = server $ Mul 12 34 $ \(product,server') ->
-  print product <> server' Terminate
+exampleClient server = server $ Mul 12 34 $ \(product,server') -> do
+  print product
+  server' Terminate
 
 exampleServer :: Server
 exampleServer client = case client of
-  Mul x y k -> k (x*y,exampleServer)
+  Mul x y respond -> respond (x*y,exampleServer)
   Terminate -> return ()
 
+
+-- See 'StorageServer.hs' for a more realistic server.
--- Examples/StorageServer.hs
+++ Examples/StorageServer.hs
@@ -1,10 +1,11 @@
 {-# LANGUAGE TypeOperators, GADTs #-}
 module StorageServer where
 
--- This module implements a storage server. The data is stored in the
--- linear heap.
+-- This module shows an example of a more realistic server
+-- implementation: a storage server.
 
--- Read the "Protocol" module first for an introduction to protocols.
+-- We show in particular how to store long-term data in the linear
+-- heap.
 
 import Data.Monoid
 
@@ -18,45 +19,69 @@ type Effect = IO () -- for simplicity
 
 type N a = a ⊸ Effect
 
+-- | A linear version of Maybe
 data Perhaps v where
   No :: Perhaps v
   Indeed :: v {-1-}-> Perhaps v
 
+-- Read the "Protocol.hs" file first for an introduction to protocols.
 data Client k v
-  = Store {-ω-}k {-1-}v {-1-}(N (Server k v))
+  = Store {-ω-}k {-1-}v {-1-}(N (Server k v)) -- (1)
   | Get {-ω-}k {-1-}(N (Perhaps v ⊗ (Server k v)))
   | Terminate
+
 type Server k v = N (Client k v)
 
--- List implementation. In reality this should be more clever.
-data Storage k v where
-  Empty :: Storage k v
-  -- Compartment :: k -> v ⊸ Storage k v ⊸ Storage k v -- GHC does not like this at the moment
-  Tray :: {-ω-}k -> v {-1-}-> Storage k v {-1-}-> Storage k v
-
-class Dup a where
-  copy :: a -> (a⊗a)
-
-find :: (Eq k, Dup v) => k -> Storage k v ⊸ (Perhaps v ⊗ Storage k v)
-find _ Empty = (No,Empty)
-find k (Tray k' v rest)
-  | k == k' = case copy v of (v,v') -> (Indeed v',Tray k v rest)
-  | otherwise = case find k rest of
-      (result,rest') -> (result,Tray k v rest')
-
-basicStorageServer :: (Eq k, Dup v) => Storage k v -> Server k v
+-- | A storage server implemented in memory
+basicStorageServer :: (Eq k, Dup v) => Storage k v ⊸ Server k v
 basicStorageServer storage client = case client  of
   Store k v respond -> respond (basicStorageServer (Tray k v storage))
+                       -- Exercise: discard 'old' entries to simulate a cache.
   Get k respond -> case find k storage of
     (x,storage') -> respond (x,basicStorageServer storage')
   Terminate -> return ()
 
+-- | Storage sturucture.
+data Storage k v where
+  Empty :: Storage k v
+  -- Tray :: k -> v ⊸ Storage k v ⊸ Storage k v -- GHC does not like this at the moment
+  Tray :: {-ω-}k -> v {-1-}-> Storage k v {-1-}-> Storage k v
+-- ... implemented as a list. In reality this should be more clever.
 
-tieredStorageServer :: Server k v -> Server k v -> Server k v
+class Dup a where
+  copy :: a -> (a⊗a)
+
+
+find :: (Eq k, Dup v) => k -> Storage k v ⊸ (Perhaps v ⊗ Storage k v)
+find _ Empty = (No,Empty)
+find k (Tray k' v rest)                                          -- (1)
+  | k == k' = case copy v of (v,v') -> (Indeed v',Tray k v rest) -- (2)
+  | otherwise = case find k rest of
+      (result,rest') -> (result,Tray k v rest')                  -- (2)
+
+-- Remark on efficiency: (1) frees stuff from the heap and (2)
+-- allocates the same amount. We can reasonably expect that an
+-- optimizer will reuse the space instead of performing two inverse heap
+-- manipulations.
+
+
+-- In a production code, the server state will inevitably contain
+-- references to (linear) contexts for database, disk, etc. See
+-- "HaskellR.hs" for how such contexts are meant to be managed.
+
+-- | Here we give an example of server combinators: we try to fetch
+-- data from the first server, but if that fails we go for the second one.
+-- We store always in both servers.
+tieredStorageServer :: Dup v => Server k v -> Server k v -> Server k v
 tieredStorageServer s1 s2 client = case client of
   Get k respond -> s1 $ Get k $ \mv -> case mv of
     (Indeed v,s1) -> respond (Indeed v,tieredStorageServer s1 s2)
     (No,s1) -> s2 $ Get k $ \mv -> case mv of
       (Indeed v,s2) -> respond (Indeed v,tieredStorageServer s1 s2)
       (No,s2) -> respond (No,tieredStorageServer s1 s2)
-  Store k v respond -> s2 $ Store k v $ \s2 -> respond (tieredStorageServer s1 s2)
+  Store k v respond -> let (v1,v2) = copy v in
+    s1 $ Stoke k v1 $ \s1 ->
+    s2 $ Store k v2 $ \s2 ->
+    respond (tieredStorageServer s1 s2)
+
+
