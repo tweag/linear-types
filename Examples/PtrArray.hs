ba0a4f7d43173892119ac191bdfc36a1ea84e5d3
YPares <yves.pares@gmail.com>
2016-10-14 15:39:08 +0200

    Added PtrArray.

--- Examples/PtrArray.hs
+++ Examples/PtrArray.hs
@@ -21,7 +21,7 @@ type N a = a ⊸ Effect
 newtype Bang a = Bang { unBang :: a }
 
 
-data MAr a = MAr {-ω-}Int {-ω-}(Ptr (Ptr a))
+data MAr a = MAr {-ω-}Int {-ω-}(Ptr a)
 
 newtype Ar a = Ar (MAr a)
 
@@ -32,11 +32,12 @@ instance Data CChar where
   move x = Bang x
   free x = ()
 
-withNewAr :: Int -> (MAr a ⊸ Bang k) ⊸ k
+withNewAr :: Storable a => Int -> (MAr a ⊸ Bang k) ⊸ k
 withNewAr n f = unBang $ unsafePerformIO $
-  allocaArray n (return . f . MAr n)
+  allocaArray n $ \ar ->
+    return $ f $ MAr n ar
 
-updateAr :: Int -> Ptr a -> MAr a ⊸ MAr a
+updateAr :: Storable a => Int -> a -> MAr a ⊸ MAr a
 updateAr n byte (MAr size ar) = unsafePerformIO $ do
   pokeElemOff ar n byte
   return $ MAr size ar
@@ -47,7 +48,7 @@ freeAr (MAr _ ar) = unsafePerformIO $ Foreign.free ar
 freezeAr :: MAr a ⊸ Bang (Ar a)
 freezeAr (MAr size ar) = Bang (Ar (MAr size ar))
 
-indexAr :: Int -> Ar a -> Ptr a
+indexAr :: Storable a => Int -> Ar a -> a
 indexAr i (Ar (MAr size ar)) = unsafePerformIO $
   peekElemOff ar i
 
