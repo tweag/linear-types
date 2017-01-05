a2a526fb88f90ce1dd8f0a9916241b3d5d17cff5
Jean-Philippe Bernardy <jeanphilippe.bernardy@gmail.com>
2017-01-05 15:30:46 +0100

    acm article infra

---
+++ ICFP2017/shell.nix
@@ -0,0 +1,22 @@
+with (import <nixpkgs> {});
+let orgEmacs = emacsWithPackages (with emacsPackagesNg; [org]);
+in stdenv.mkDerivation {
+  name = "docsEnv";
+  buildInputs = [ # orgEmacs
+                  haskellPackages.lhs2tex
+                  biber
+                  (texlive.combine {
+                    inherit (texlive)
+                      # collection-fontutils
+                      # tex-gyre tex-gyre-math
+                      lm
+                      todonotes
+                      stmaryrd lazylist polytable # for lhs2tex
+                      xargs
+                      biblatex
+                      logreq
+                      scheme-small wrapfig marvosym wasysym wasy cm-super unicode-math filehook lm-math capt-of
+                      xstring ucharcat;
+                  })
+                ];
+}
