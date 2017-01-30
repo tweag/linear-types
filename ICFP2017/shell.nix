156b64b58fb9520cb20fac843b0557846ead91a3
Ryan Newton <rrnewton@gmail.com>
2017-01-30 15:43:53 -0500

    minor shell.nix tweak

--- ICFP2017/shell.nix
+++ ICFP2017/shell.nix
@@ -1,4 +1,9 @@
+# Non-reproducible "current" version:
 with (import <nixpkgs> {});
+# This works for RRN:
+# with import (fetchTarball https://github.com/NixOS/nixpkgs-channels/archive/nixos-16.09.tar.gz) {};
+
+
 let orgEmacs = emacsWithPackages (with emacsPackagesNg; [org]);
 in stdenv.mkDerivation {
   name = "docsEnv";
