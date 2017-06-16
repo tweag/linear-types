0f52de89976701e917a27048e1554052a615c48e
Arnaud Spiwack <arnaud@spiwack.net>
2017-06-16 19:15:40 +0200

    A few things towards bytearray implementation

--- Examples/src/ByteArray.hs
+++ Examples/src/ByteArray.hs
@@ -1,18 +1,37 @@
+{-# LANGUAGE BangPatterns #-}
+
 module ByteArray where
 
+import Data.ByteString (ByteString)
+import qualified Data.ByteString as ByteString
+import qualified Data.ByteString.Unsafe as ByteString (unsafePackMallocCStringLen)
+import Data.Word
+import Foreign.C.String
+import Foreign.Storable
 import Linear.Std
+import Linear.Unsafe
+import System.IO.Unsafe (unsafeDupablePerformIO)
 
-type WByteArray = ()
-type ByteArray = ()
+data WByteArray = WBA { pos::CString, orig::CString, stored::Int, rem::Int }
+type ByteArray = ByteString
 
 alloc :: Int -> (WByteArray ⊸ Unrestricted b) ⊸ Unrestricted b
 alloc = undefined
 
-writeWord :: Word -> WByteArray ⊸ ()
-writeWord = undefined
+writeByte :: Word8 -> WByteArray ⊸ WByteArray
+writeByte = undefined
+
+writeStorable :: Storable a => a -> WByteArray ⊸ WByteArray
+writeStorable = undefined
 
 freeze :: WByteArray ⊸ Unrestricted ByteArray
-freeze = undefined
+freeze wba =
+  Unrestricted $ unsafeDupablePerformIO $
+    ByteString.unsafePackMallocCStringLen (orig,len)
+  where
+    project (WBA{orig=orig, stored=len}) = (orig,len)
+    promote :: WByteArray ⊸ Unrestricted (orig,len)
+    promote wba = undefined
 
-index :: ByteArray -> Word -> a
+index :: ByteArray -> Int -> Word8
 index = undefined
