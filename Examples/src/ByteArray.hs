95a44b6de266d17dd88977e2f4883c9720b4fe44
Arnaud Spiwack <arnaud@spiwack.net>
2017-06-20 16:29:12 +0200

    Examples: Frozen ByteArray are just ByteString

--- Examples/src/ByteArray.hs
+++ Examples/src/ByteArray.hs
@@ -16,7 +16,6 @@ import Prelude hiding (rem)
 import System.IO.Unsafe (unsafeDupablePerformIO)
 
 data WByteArray = WBA { pos::CString, orig::CString, stored::Int, rem::Int }
-type ByteArray = ByteString
 
 alloc :: Int -> (WByteArray ⊸ Unrestricted b) ⊸ Unrestricted b
 alloc i f = f $ unsafeDupablePerformIO $ do
@@ -33,12 +32,11 @@ writeStorable obj wbarr = write (unsafeUnrestricted wbarr)
     write :: Unrestricted WByteArray ⊸ WByteArray
     write (Unrestricted wba) = unsafeDupablePerformIO $ do
       let size = sizeOf obj
-      let align = alignment obj
       poke (castPtr (pos wba)) obj
-      let newPos = alignPtr (pos wba `plusPtr` size) align
+      let newPos = pos wba `plusPtr` size -- no alignment is done because it's hard to read back
       return $ wba { pos = newPos, rem = rem wba - (newPos `minusPtr` pos wba) }
 
-freeze :: WByteArray ⊸ Unrestricted ByteArray
+freeze :: WByteArray ⊸ Unrestricted ByteString
 freeze wba =
     pack (project (promote wba))
   where
@@ -48,9 +46,6 @@ freeze wba =
     promote :: WByteArray ⊸ Unrestricted WByteArray
     promote w = unsafeUnrestricted w
 
-    pack :: Unrestricted CStringLen ⊸ Unrestricted ByteArray
+    pack :: Unrestricted CStringLen ⊸ Unrestricted ByteString
     pack (Unrestricted cstr) = Unrestricted $ unsafeDupablePerformIO $
         ByteString.unsafePackMallocCStringLen cstr
-
-index :: ByteArray -> Int -> Word8
-index = ByteString.index
