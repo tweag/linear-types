6773d7b21ed816a282461ffaddbcdc6097cc5c78
Ryan Newton <rrnewton@gmail.com>
2017-06-22 05:14:07 -0400

    More of the previous -- represent a cursor as a sequence of bytestrings

--- Examples/src/Cursors/PureStorable.hs
+++ Examples/src/Cursors/PureStorable.hs
@@ -23,29 +23,37 @@ module Cursors.PureStorable
 
 import Linear.Std (Unrestricted(..))
 import Linear.Unsafe (unsafeCastLinear, unsafeCastLinear2, unsafeUnrestricted)
-      
+
+import Data.Sequence      
 import Data.Monoid
 import Data.Word
 import Data.Int
 import Data.ByteString.Char8   as BS
-import Data.ByteString.Internal (fromForeignPtr)
+import Data.ByteString.Internal (fromForeignPtr, toForeignPtr)
 import Data.Binary (get, put, Binary, encode, decode, getWord8)
 import Data.Binary.Get (runGetOrFail, Get)
 import Data.Binary.Put (execPut)
 import Foreign.Storable
 import Foreign.Ptr ()
-import Foreign.ForeignPtr (withForeignPtr, mallocForeignPtr, castForeignPtr)
+import Foreign.ForeignPtr (ForeignPtr, withForeignPtr, mallocForeignPtr, castForeignPtr)
 import Foreign.Marshal.Alloc (alloca)
 import System.IO.Unsafe (unsafeDupablePerformIO)
 
--- | 
-serialize :: Storable a => a -> ByteString
-serialize x = unsafeDupablePerformIO $ do
+-- | A hack for treating Storable as a pure, rather than IO-based interface.
+serialize :: Storable a => a ⊸ ByteString
+serialize = unsafeCastLinear
+ (\x -> unsafeDupablePerformIO $ do
    fp <- mallocForeignPtr
    withForeignPtr fp (\p -> poke p x)
-   return $! fromForeignPtr (castForeignPtr fp) 0 (sizeOf x)
+   return $! fromForeignPtr (castForeignPtr fp) 0 (sizeOf x))
 
-{-
+deserialize :: forall a . Storable a => ByteString ⊸ a
+deserialize = unsafeCastLinear
+ (\bs -> unsafeDupablePerformIO $ do
+   let (fp,st,_) = toForeignPtr bs
+       fp' :: ForeignPtr a
+       fp' = castForeignPtr fp
+   withForeignPtr fp (`peekByteOff` st))
 
 -- Cursor Types:
 --------------------------------------------------------------------------------
@@ -53,11 +61,11 @@ serialize x = unsafeDupablePerformIO $ do
 -- | A "needs" cursor requires a list of fields be written to the
 -- bytestream before the data is fully initialized.  Once it is, a
 -- value of the (second) type parameter can be extracted.
-newtype Needs (l :: [*]) t = Needs Builder
+newtype Needs (l :: [*]) t = Needs (Seq ByteString)
 
 -- | A "has" cursor is a pointer to a series of consecutive,
 -- serialized values.  It can be read multiple times.
-newtype Has (l :: [*]) = Has ByteString
+newtype Has (l :: [*]) = Has (Seq ByteString)
   deriving Show
 
 -- | A packed value is very like a singleton Has cursor.  It
@@ -68,12 +76,16 @@ newtype Packed a = Packed ByteString
 
 -- Cursor interface
 --------------------------------------------------------------------------------
-         
+
+consR :: Seq a ⊸ a ⊸ Seq a
+consR = unsafeCastLinear2 (|>)
+
 -- | Write a value to the cursor.  Write doesn't need to be linear in
 -- the value written, because that value is serialized and copied.
-writeC :: Binary a => a -> Needs (a ': rst) t ⊸ Needs rst t
-writeC a (Needs bld1) = Needs (bld1 `app` execPut (put a))
+writeC :: Storable a => a -> Needs (a ': rst) t ⊸ Needs rst t
+writeC a (Needs s) = Needs (s `consR` serialize a)
 
+{-
 -- | Reading from a cursor scrolls past the read item and gives a
 -- cursor into the next element in the stream:
 readC :: Binary a => Has (a ': rst) -> (a, Has rst)
