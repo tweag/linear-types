a6deb88ec17aa9407e12c3f8f80342923bb0bb34
Arnaud Spiwack <arnaud@spiwack.net>
2017-06-23 14:41:34 +0200

    Minimal array builder implementation

--- Examples/src/FreezeArray.hs
+++ Examples/src/FreezeArray.hs
@@ -1,3 +1,6 @@
+{-# LANGUAGE Rank2Types #-}
+{-# LANGUAGE LambdaCase #-}
+
 module FreezeArray where
 
 import Linear.Std
@@ -10,6 +13,9 @@ data MArray a = MArray -- TODO
 newMArray :: Int -> a -> (MArray a ⊸ Unrestricted b) ⊸ Unrestricted b
 newMArray = error "TODO: FreezeArray.newMArray"
 
+lengthM :: MArray a ⊸ (MArray a, length)
+lengthM = error "TODO: FreezeArray.lengthM"
+
 write :: MArray a ⊸ Int -> a -> MArray a
 write = error "TODO: FreezeArray.write"
 
@@ -21,3 +27,27 @@ freeze = error "TODO: FreezeArray.freeze"
 
 index :: Array a -> Int -> a
 index = error "TODO: FreezeArray.index"
+
+
+-- * Builder API
+
+data BuilderInstr a r
+  = Step ((a -> Builder a) ⊸ r)
+  | Freeze (Unrestricted (Array a) ⊸ r)
+
+newtype Builder a = Builder (forall r. BuilderInstr a r ⊸ r)
+
+push :: a -> Builder a ⊸ Builder a
+push a (Builder exec) = exec $ Step (\ step -> step a)
+
+build :: Int -> a -> (Builder a ⊸ Unrestricted b) ⊸ Unrestricted b
+build chunkSize initVal k =
+    newMArray chunkSize initVal $ \ marr ->
+      k (mkBuilder 0 marr)
+
+  where
+    mkBuilder :: Int -> MArray a ⊸ Builder a
+    mkBuilder i marr = Builder (\case   -- No ($) because the linear ($) doesn't work with polymorphic types
+      Step doStep -> doStep $ \ a ->
+        mkBuilder (i+1) (write marr i a)
+      Freeze doFreeze -> doFreeze (freeze marr))
