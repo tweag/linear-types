24a152fa5c2ecab854c917a684d3f427daa03d71
Ryan Newton <rrnewton@gmail.com>
2017-06-23 06:44:02 -0400

    Confirm that only freeze/alloc need to be NOINLINE.  INLINE and unsafeDupable appear safe for the raw read/write operations.

--- Examples/src/ByteArray.hs
+++ Examples/src/ByteArray.hs
@@ -2,6 +2,8 @@
 -- | An interface for mutable byte arrays, handled linearly.
 
 {-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE MagicHash #-}
+{-# LANGUAGE UnboxedTuples #-}
 
 module ByteArray
     ( WByteArray, alloc, freeze, headStorable, writeStorable, writeByte )
@@ -21,29 +23,38 @@ import Linear.Unsafe
 import Prelude hiding (rem,($))
 import System.IO.Unsafe (unsafePerformIO, unsafeDupablePerformIO)
 
+import GHC.IO
+import GHC.Prim (RealWorld, State#)
+---------------------------------
 
-data WByteArray = WBA { pos :: !CString, orig :: !CString, stored :: !Int, rem :: !Int }
+type Token = State# RealWorld
+
+data WByteArray = WBA { pos :: !CString, orig :: !CString
+                      , stored :: !Int, rem :: !Int
+--                      , state :: Token
+                      }
 
 {-# NOINLINE alloc #-}
 -- | Allocate and use a mutable, linear byte array.
 alloc :: Int -> (WByteArray ⊸ Unrestricted b) ⊸ Unrestricted b
--- Todo: @alloc@ should handle exception and free the pointer it allocates.
 alloc i f = forceUnrestricted $ f $ unsafePerformIO $ do
-  str <- mallocBytes (i+1) -- Remark: can't use @alloca@ as the pointer will usually survive the scope
-  pokeElemOff str i (0::CChar) -- Null terminated.
-  return $! WBA { pos = str, orig = str, stored = 0, rem = i }
+     str <- mallocBytes (i+1) -- Remark: can't use @alloca@ as the pointer will usually survive the scope
+     pokeElemOff str i (0::CChar) -- Null terminated.
+     return $! WBA{ pos = str, orig = str, stored = 0, rem = i }
+-- Todo: @alloc@ should handle exception and free the pointer it allocates.
+
 
 -- | Write a single byte to the end of a byte array.
 writeByte :: Word8 -> WByteArray ⊸ WByteArray
 writeByte = writeStorable
 
-{-# NOINLINE writeStorable #-}
+{-# INLINE writeStorable #-}
 -- | Write a storable value to the end of a byte array.
 writeStorable :: Storable a => a -> WByteArray ⊸ WByteArray
 writeStorable obj wbarr = write (unsafeUnrestricted wbarr)
   where
     write :: Unrestricted WByteArray ⊸ WByteArray
-    write (Unrestricted wba) = wba `seq` unsafePerformIO $ do
+    write (Unrestricted wba) = unsafeDupablePerformIO $ do
       let size = sizeOf obj
       poke (castPtr (pos wba)) obj
       let newPos = pos wba `plusPtr` size -- no alignment is done because it's hard to read back
@@ -62,9 +73,9 @@ freeze wba =
     pack (Unrestricted cstr) = Unrestricted $ unsafePerformIO $
         ByteString.unsafePackMallocCStringLen cstr
 
-{-# NOINLINE headStorable #-}
+{-# INLINE headStorable #-}
 -- TODO: bound checking
 headStorable :: Storable a => ByteString -> a
-headStorable bs = unsafePerformIO $
+headStorable bs = unsafeDupablePerformIO $
     ByteString.unsafeUseAsCString bs $ \ cstr ->
       peek (castPtr cstr)
