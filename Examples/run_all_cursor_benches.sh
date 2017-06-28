13741d2d6c89a19bba570cc121de1770bc782256
Ryan Newton <rrnewton@gmail.com>
2017-06-28 14:25:26 -0400

    Gather more info from criterion runs.  File away in a uniquely named dir

--- Examples/run_all_cursor_benches.sh
+++ Examples/run_all_cursor_benches.sh
@@ -8,31 +8,42 @@ stack bench --no-run-benchmarks
 # Find the executable, and assume it runs OUTSIDE docker:
 EXE=`stack exec -- which bench-cursor`
 CRIT="./bin/criterion-interactive"
-
 set +x
 
 echo "Using binary: $EXE"
 
-for name in sumtree maptree;
-do
-  for (( depth=1; depth <= 20; depth++ ))
+RUNDIR=run_`hostname -s`_`date +"%s"`
+
+function go() {
+  echo "Running benchmarks with output to dir: $RUNDIR"
+    
+  for name in sumtree maptree;
   do
-    echo
-    echo "Benchmarking tree depth $depth"
-    echo "=============================="
-    for variant in boxed packed ST-packed unpack-repack;
+    for (( depth=1; depth <= 20; depth++ ))
     do
-        echo "Variant: $variant"
-        echo "-----------------"
-        echo "START_BENCHMARK"
-        echo "PROGNAME: $name"
-        echo "VARIANT: $variant"
-        echo "ARGS: $depth"
-        set -x
-        $CRIT $EXE $name $variant $depth -- -o report_${variant}_${depth}.html
-        set +x
-        echo "END_BENCHMARK"
-        echo
+      echo
+      echo "Benchmarking tree depth $depth"
+      echo "=============================="
+      for variant in boxed packed ST-packed unpack-repack;
+      do
+          echo "Variant: $variant"
+          echo "-----------------"
+          echo "START_BENCHMARK"
+          echo "PROGNAME: $name"
+          echo "VARIANT: $variant"
+          echo "ARGS: $depth"
+          set -x
+          $CRIT $EXE $name $variant $depth -- \
+                -o     $RUNDIR/report_${variant}_${depth}.html \
+                --json $RUNDIR/report_${variant}_${depth}.json \
+                --csv  $RUNDIR/report_${variant}_${depth}.csv
+          set +x
+          echo "END_BENCHMARK"
+          echo
+      done;
     done;
   done;
-done;
+}
+
+mkdir -p $RUNDIR    
+go 2> /dev/stdout | tee $RUNDIR/full_benchmark_log.txt
